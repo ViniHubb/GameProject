a63627feff5c1fe964eed2ffdc2836b2512144dd
vinicius <vininiquini7@gmail.com>
2023-09-19 22:13:08 -0300

    Alteração na interface de interação do usuário

--- src/Feedback/index.js
+++ src/Feedback/index.js
@@ -1,8 +1,17 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { View, Text, Button } from "react-native"
 import styles from "./styles"
 
 export default function Feedback({ navigation, route }) {
+
+  const [able, setAble] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAble(false)
+    },2000)
+  },[route.params.renome])
+
   return (
     <View style={styles.boxFeedback}>
       {route.params.misses || route.params.hits ?
@@ -13,19 +22,36 @@ export default function Feedback({ navigation, route }) {
           <Button
             title="OK"
             onPress={() => navigation.navigate("Main")}
+            disabled={able}
           />
         </View>
         :
         <View>
-          <Text>Parabéns {route.params.renome}, você acertou</Text>
-          <View style={styles.escolhas}>  
-            <Text>Holmes: {route.params.holmesP}</Text>
-            <Text>Watson: {route.params.watsonP}</Text>
-          </View>
-          <Button
-            title="PROXIMO"
-            onPress={() => navigation.navigate("Main")}
-          />
+          {route.params.superHits < 0 ?
+            <View>
+              <Text>A bomba explodiu, parabéns {route.params.renome}, você perdeu</Text>
+              <View style={styles.escolhas}>
+                <Text>Holmes: {route.params.holmesP}</Text>
+                <Text>Watson: {route.params.watsonP}</Text>
+              </View>
+              <Button
+                title="PROXIMO"
+                onPress={() => navigation.navigate("Main")}
+              />
+            </View>
+            :
+            <View>
+              <Text>Parabéns {route.params.renome}, você acertou</Text>
+              <View style={styles.escolhas}>
+                <Text>Holmes: {route.params.holmesP}</Text>
+                <Text>Watson: {route.params.watsonP}</Text>
+              </View>
+              <Button
+                title="PROXIMO"
+                onPress={() => navigation.navigate("Main")}
+              />
+            </View>
+          }
         </View>
       }
     </View>
