3fa5b4f4c00333a6cb16bda1ed65c5f430927c94
vinicius <vininiquini7@gmail.com>
2023-10-06 11:12:14 -0300

    Modificações visuais nos botões

--- src/Main/Components/Keyboard/index.js
+++ src/Main/Components/Keyboard/index.js
@@ -1,174 +1,190 @@
-import React, { useEffect, useState } from "react"
-import { View, Text, TouchableOpacity, Button, Image } from "react-native"
+import React, { useState } from "react"
+import { View, Text, Button, Image, Pressable, ImageBackground } from "react-native"
 import styles from "./styles"
 
 export default function Keyboard(props) {
 
-  const botao = require('C:/Users/Vinicius/Documents/Projects/GameProject/src/Images/sombra.png')
-  const botaoPress = require('C:/Users/Vinicius/Documents/Projects/GameProject/src/Images/sombraPress.png')
+  const botao = require('C:/Users/Vinicius/Documents/Projects/GameProject/src/Images/newBotaoBorda.png')
+  const botaoPress = require('C:/Users/Vinicius/Documents/Projects/GameProject/src/Images/newBotao.png')
   const [botoes, setBotoes] = useState([botao,botao,botao,botao,botao,botao,botao,botao,botao,botao,botao,botao,botao])
 
-  function press(number) {        // Funcao para garantir que o numero so seja apertado uma vez
-    console.log('Test sucess');
+  function press(number) {
     let newBotoes = [...botoes]
     newBotoes[number] = botaoPress
     setBotoes(newBotoes)
   }
 
-  function desPress(){
-    console.log('Test sucess 222');
+  function desPress(number) {
     let newBotoes = [...botoes]
-    newBotoes[props.tenta[props.tenta.length -1]] = botao
-    setBotoes(newBotoes)
+    if(number) {
+      newBotoes[10] = botao
+      newBotoes[props.tenta[props.tenta.length -1]] = botao
+      setBotoes(newBotoes)
+    } else {
+      for(let i=0; i < 10; i++) {
+        newBotoes[i] = botao
+        setBotoes(newBotoes)
+      }
+    }
   }
 
   return (
-    <View style={styles.boxButtons}>
-      <View style={styles.line}>
-      <TouchableOpacity       // 0
-          onPress={() => {
-            props.bota(0)
-          }}
-          disabled={props.ver(0)}
-          style={[styles.testes]}
-          activeOpacity={1}
-        >
-          <Image resizeMode="stretch" source={require('C:/Users/Vinicius/Documents/Projects/GameProject/src/Images/sombra.png')}
-          style={{width:30, height: 25, backgroundColor: "white"}}/>
-        </TouchableOpacity>
-        <TouchableOpacity       // 1
-          onPress={() => {
-            props.bota(1)
-          }}
-          activeOpacity={1}
-          disabled={props.ver(1)}
-          style={[styles.testes]}       
-        >
-          <Image resizeMode="stretch" source={require('C:/Users/Vinicius/Documents/Projects/GameProject/src/Images/sombraPress.png')} 
-          style={{width:30, height: 25, backgroundColor: "blue"}}/>
-        </TouchableOpacity>
-        <TouchableOpacity       // 2
-          onPress={() => {
-            props.bota(2)
-          }}
-          disabled={props.ver(2)}
-          style={[styles.testes]}     
-        >
-          <Image resizeMode="stretch" source={require('C:/Users/Vinicius/Documents/Projects/GameProject/src/Images/sombra2.png')} 
-            style={{width:30, height: 25, backgroundColor: "red"}}/>
-        </TouchableOpacity>
-        <TouchableOpacity     // 3
-          onPress={() => {
-            props.bota(3)
-          }}
-          disabled={props.ver(3)}
-          style={[styles.testes]}        
-        >
-          <Image resizeMode="stretch" source={require('C:/Users/Vinicius/Documents/Projects/GameProject/src/Images/sombraPress2.png')} 
-            style={{width:30, height: 25, backgroundColor: "green"}}/>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.line}>
-        <TouchableOpacity       // 4
-          onPress={() => {
-            props.bota(4)
-            press(4)
-          }}
-          disabled={props.ver(4)}
-          style={[styles.testes]}
-        >
-          <Image resizeMode="stretch" source={botoes[4]} 
-            style={{width:30, height: 25, backgroundColor: "yellow"}}/>
-          </TouchableOpacity>
-        <TouchableOpacity       // 5
-          onPress={() => {
-            props.bota(5)
-            press(5)
-          }}
-          disabled={props.ver(5)}
-          style={[styles.testes]}        
-        >
-          <Image resizeMode="stretch" source={botoes[5]} 
-            style={{width:30, height: 25, backgroundColor: "brown"}}/>
-        </TouchableOpacity>
-        <TouchableOpacity       // 6
-          onPress={() => {
-            props.bota(6)
-            press(6)
-          }}
-          disabled={props.ver(6)}
-          style={[styles.testes]}      
-        >
-          <Image resizeMode="stretch" source={botoes[6]} 
-            style={{width:30, height: 25, backgroundColor: "gray"}}/>
-        </TouchableOpacity>
-        <TouchableOpacity       // 7
-          onPress={() => {
-            props.bota(7)
-            press(7)
-          }}
-          disabled={props.ver(7)}
-          style={[styles.testes]}
-        >
-          <Image resizeMode="stretch" source={botoes[7]} 
-            style={{width:30, height: 25, backgroundColor: "purple"}}/>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.line}>
-        <TouchableOpacity       // Delet
-          onPress={() => {
-            props.dell()
-            desPress()
-          }}
-          style={[styles.testes]}
-        >
-          <Image resizeMode="stretch" source={botoes[10]} 
-            style={{width:30, height: 25, backgroundColor: "darkslategrey"}}/>
-        </TouchableOpacity>
-        <TouchableOpacity       // 8
-          onPress={() => {
-            props.bota(8)
-            press(8)
-          }}
-          disabled={props.ver(8)}
-          style={[styles.testes]}
-        >
-          <Image resizeMode="stretch" source={botoes[8]} 
-            style={{width:30, height: 25, backgroundColor: "violet"}}/>
-        </TouchableOpacity>
-        <TouchableOpacity       // 9
-          onPress={() => {
-            props.bota(9)
-            press(9)
-          }}
-          disabled={props.ver(9)}
-          style={[styles.testes]}
-        >
-          <Image resizeMode="stretch" source={botoes[9]} 
-            style={{width:30, height: 25, backgroundColor: "aqua"}}/>
-        </TouchableOpacity>
-        <TouchableOpacity       // Go!
-          onPress={() => {
-              props.save()
+    <View style={{flex:1}}>
+      <ImageBackground resizeMode="stretch" source={require("C:/Users/Vinicius/Documents/Projects/GameProject/src/Images/placa4.png")}
+      style={styles.boxButtons}>
+        <View style={styles.line}>
+          <ImageBackground resizeMode="stretch" source={botoes[0]}
+                style={[styles.buttons, {backgroundColor:"white"}]}>
+            <Pressable       // 0
+              onPress={() => {
+                props.bota(0)
+                press(0)
+              }}
+              disabled={props.ver(0)}
+              style={[styles.testes]}
+            >
+              {/* <Text></Text> */}
+            </Pressable>
+          </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[1]}
+                style={[styles.buttons, {backgroundColor:"blue"}]}>
+            <Pressable       // 1
+              onPress={() => {
+                props.bota(1)
+                press(1)
+              }}
+              disabled={props.ver(1)}
+              style={[styles.testes]}
+            ></Pressable>
+          </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[2]}
+                style={[styles.buttons, {backgroundColor:"red"}]}>
+            <Pressable       // 2
+              onPress={() => {
+                props.bota(2)
+                press(2)
+              }}
+              disabled={props.ver(2)}
+              style={[styles.testes]}
+            ></Pressable>
+          </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[3]}
+                style={[styles.buttons, {backgroundColor:"green"}]}>
+            <Pressable       // 3
+              onPress={() => {
+                props.bota(3)
+                press(3)
+              }}
+              disabled={props.ver(3)}
+              style={[styles.testes]}
+            ></Pressable>
+          </ImageBackground>
+        </View>
+        <View style={styles.line}>
+          <ImageBackground resizeMode="stretch" source={botoes[4]}
+          style={styles.buttons}>
+            <Pressable       // 4
+              onPress={() => {
+                props.bota(4)
+                press(4)
+              }}
+              disabled={props.ver(4)}
+              style={[styles.buttons]}
+            ></Pressable>
+          </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[5]}
+          style={styles.buttons}>
+            <Pressable       // 5
+              onPress={() => {
+                props.bota(5)
+                press(5)
+              }}
+              disabled={props.ver(5)}
+              style={[styles.buttons]}
+            ></Pressable>
+          </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[6]}
+          style={styles.buttons}>
+            <Pressable       // 6
+              onPress={() => {
+                props.bota(6)
+                press(6)
+              }}
+              disabled={props.ver(6)}
+              style={[styles.buttons]}
+            ></Pressable>
+          </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[7]}
+          style={styles.buttons}>
+            <Pressable       // 7
+              onPress={() => {
+                props.bota(7)
+                press(7)
+              }}
+              disabled={props.ver(7)}
+              style={[styles.buttons]}
+            ></Pressable>
+          </ImageBackground>
+        </View>
+        <View style={styles.line}>
+          <ImageBackground resizeMode="stretch" source={botoes[10]}  // botoes[10]
+            style={styles.buttons}>
+              <Pressable       // Delet
+                onPressIn={() => {
+                  press(10)
+                }}
+                onPressOut={() => {
+                  desPress(1)
+                  props.dell()
+                }}
+                style={[styles.testes]}
+              ></Pressable>
+            </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[8]}
+          style={styles.buttons}>
+            <Pressable       // 8
+              onPress={() => {
+                props.bota(8)
+                press(8)
+              }}
+              disabled={props.ver(8)}
+              style={[styles.buttons]}
+            ></Pressable>
+          </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[9]}
+          style={styles.buttons}>
+            <Pressable       // 9
+              onPress={() => {
+                props.bota(9)
+                press(9)
+              }}
+              disabled={props.ver(0)}
+              style={[styles.buttons]}
+            ></Pressable>
+          </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[11]}  // botoes[11]
+          style={styles.buttons}>
+            <Pressable       // Go!
+            onPress={() => {
+                props.save()
+                desPress(0)
+              }}
+            disabled={!props.ver(-1)}
+            style={[styles.buttons]}
+            ></Pressable>
+          </ImageBackground>
+          <ImageBackground resizeMode="stretch" source={botoes[0]}  // botoes[0]
+          style={styles.buttons}>
+            <Pressable       // Clear
+            onPress={() => {
+              props.limpa()
             }}
-          disabled={!props.ver(-1)}
-          style={[styles.testes]}
-        >
-          <Image resizeMode="stretch" source={botoes[11]} 
-            style={{width:30, height: 25, backgroundColor: "lightgreen"}}/>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.line}>
-        <TouchableOpacity       // Clear
-          onPress={() => {
-            props.limpa()
-          }}
-          style={styles.buttons}
-        >
-          <Image resizeMode="stretch" source={botoes[12]} 
-            style={{width:50, height: 25, backgroundColor: "white"}}/>
-        </TouchableOpacity>
-      </View>
+            style={styles.buttons}
+          >
+          </Pressable>
+          </ImageBackground>
+        </View>
+      </ImageBackground>
     </View>
   )
 }
--- src/Main/Components/Keyboard/styles.js
+++ src/Main/Components/Keyboard/styles.js
@@ -2,30 +2,25 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   boxButtons:{
-    flex: 1,
+    flex:1,
     alignItems: "center",
-    backgroundColor: "black",
-    paddingTop: 20,
-    marginTop: 2
+    paddingTop: 20
   },
   line:{
     width: "90%",
     justifyContent: "space-around",
     flexDirection: "row",
-    padding: 5
   },
   buttons:{
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-    borderRadius: 20,
-    marginBottom: 10
+    backgroundColor: "white",
+    paddingVertical: 0,
+    paddingHorizontal: 0,
+    borderRadius: 20  
   },
   testes:{
-    //backgroundColor:"white",
-    paddingTop:5,
-    borderRadius: 20,
-    marginBottom: 10,
-    borderRadius: 100
+    //backgroundColor: "blue",
+    paddingVertical: 20,
+    paddingHorizontal: 20,
   }
 })
 
